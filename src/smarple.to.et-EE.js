c9f6cb5a533ef5bdc33a802e8e04e852d24b4423
onitecsoft <onitec@onitecsoft.com>
2015-05-27 16:49:28 +0300

    Create smarple.to.et-EE.js

---
+++ src/smarple.to.et-EE.js
@@ -0,0 +1,23 @@
+Number.culture = {
+	delimiters: {
+		group: ' ',
+		decimal: ','
+	},
+	patterns: {
+		n: ',.######',
+		n0: ',',
+		n2: ',.00',
+		n3: ',.000'
+	}
+}
+
+Date.culture = {
+	ampm: false,
+	am: 'AM',
+	pm: 'PM',
+	patterns: {
+		d: 'd.MM.yyyy',
+		t: 'hh:mm:ss',
+		dt: 'd.MM.yyyy hh:mm:ss'
+	}
+}
