905718a52b6f1f21cb83eb329b3f4c4f2f81c856
Onitec <onitec@onitecsoft.com>
2016-07-29 11:28:10 +0300

    Create smarti.to.et-EE.js

---
+++ src/smarti.to.et-EE.js
@@ -0,0 +1,27 @@
+smarti.culture = {
+	number: {
+		group: ' ',
+		decimal: ',',
+		patterns: {
+			n: ',.######',
+			n0: ',',
+			n2: ',.00',
+			n3: ',.000'
+		}
+	},
+	date: {
+		ampm: false,
+		am: '',
+		pm: '',
+		patterns: {
+			d: 'd.MM.yyyy',
+			t: 'hh:mm:ss',
+			dt: 'd.MM.yyyy hh:mm:ss'
+		},
+		a: ['P', 'E', 'T', 'K', 'N', 'R', 'L'],
+		aa: ['püh', 'esm', 'tei', 'kol', 'nel', 'ree', 'lau'],
+		aaa: ['pühapäev', 'esmaspäev', 'teisipäev', 'kolmapäev', 'neljapäev', 'reede', 'laupäev'],
+		MMM: ['jaan', 'veebr', 'märts', 'apr', 'mai', 'juuni', 'juuli', 'aug', 'sept', 'okt', 'nov', 'dets'],
+		MMMM: ['jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember']
+	}
+}
